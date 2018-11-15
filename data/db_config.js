9c8d4f4e6d08c73c38f3d9b8a575f95a0b1af22f
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-11-15 09:36:13 -0500

    initial commit

---
+++ data/db_config.js
@@ -0,0 +1,16 @@
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+
+// Connect to our Database and handle any bad connections
+mongoose.connect(process.env.MONGO_URI, {
+	auth:{
+		user: process.env.DB_USER, 
+		pass: process.env.DB_PASS
+	}
+}).then(() => {
+	console.log("Connected to Database")
+}).catch((err) =>{
+	return console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
