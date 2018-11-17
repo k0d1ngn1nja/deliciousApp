48924c20b3ac2fd355f25c55d8d3301c0fd4acd3
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-11-17 15:23:37 -0500

    implemented flashes

--- controllers/storeCntrl.js
+++ controllers/storeCntrl.js
@@ -1,7 +1,9 @@
-const Store = require("../models/Store");
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
 
 const storeContrl = {
 	homePage: (req, res, next) =>{
+		req.flash("info", "Something happened!!");
   	return res.render("index", {title: "Home | ThatsDeliciousApp"});
 	},
 
@@ -9,9 +11,11 @@ const storeContrl = {
 		res.render("store/form", {title: "Add Store"});
 	},
 
-	create: (req, res, next) =>{
-		console.log(req.body);
-		// res.json(req.body);
+	create: async (req, res, next) =>{
+		const store = new Store(req.body);
+		await store.save();
+		req.flash("success", `Successfully Created ${store.name}. Care to lease a review?`);
+		res.redirect("/");
 	},
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const storeCntrl = require("../controllers/storeCntrl");
+const { catchErrors } = require("../handlers/errorHandlers");
 
 router.get('/', storeCntrl.homePage);
 
 router.get('/add-store', storeCntrl.addStore);
 
-router.post('/add-store', storeCntrl.create);
+router.post('/add-store', catchErrors(storeCntrl.create));
 
 module.exports = router;
